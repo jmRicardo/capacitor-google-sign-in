5eb026bacaa47204b7eabfcfa4093b0e9980091f
jmRicardo <juanmanricardo@gmail.com>
2023-11-19 01:10:03 -0300

    Initial commit

---
+++ android/src/main/java/com/jmricardo/google/auth/GoogleSignIn.java
@@ -0,0 +1,11 @@
+package com.jmricardo.google.auth;
+
+import android.util.Log;
+
+public class GoogleSignIn {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
---
+++ ios/Plugin/GoogleSignInPlugin.swift
@@ -0,0 +1,18 @@
+import Foundation
+import Capacitor
+
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitorjs.com/docs/plugins/ios
+ */
+@objc(GoogleSignInPlugin)
+public class GoogleSignInPlugin: CAPPlugin {
+    private let implementation = GoogleSignIn()
+
+    @objc func echo(_ call: CAPPluginCall) {
+        let value = call.getString("value") ?? ""
+        call.resolve([
+            "value": implementation.echo(value)
+        ])
+    }
+}
